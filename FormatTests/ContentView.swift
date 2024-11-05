28ff88ec8a065ea5a659757728f59da40ae93aa6
Sarah Reichelt <sarah@troz.net>
2024-11-06 09:57:55 +1000

    Initial Commit

---
+++ FormatTests/ContentView.swift
@@ -0,0 +1,19 @@
+// Created on 6/11/2024.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FormatTests/FormatTestsApp.swift
@@ -0,0 +1,12 @@
+// Created on 6/11/2024.
+
+import SwiftUI
+
+@main
+struct FormatTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
